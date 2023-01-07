a1d817ffb0c9aafe67da77bd119d03ac85bab5d0
f1shy-dev <56125930+f1shy-dev@users.noreply.github.com>
2023-01-07 16:06:03 +0000

    new window

--- tauri-app/src/main.js
+++ tauri-app/src/main.js
@@ -3,8 +3,25 @@ import { authenticate } from "./scripts/authenticate.js";
 import { getAnisette } from "./scripts/anisette.js";
 import { initWizard } from "./scripts/wizard.js";
 import { getDevices } from "./scripts/device.js";
+const { appWindow } = window.__TAURI__.window;
 
 (async () => {
+  document
+    .getElementById("titlebar-minimize")
+    .addEventListener("click", () => appWindow.minimize());
+
+  const noDragSelector = "input, a, button"; // CSS selector
+  document
+    .querySelector(".drag-region")
+    .addEventListener("mousedown", async (e) => {
+      if (e.target.closest(noDragSelector)) return; // a non-draggable element either in target or its ancestors
+      await appWindow.startDragging();
+    });
+
+  document
+    .getElementById("titlebar-close")
+    .addEventListener("click", () => appWindow.close());
+
   initWizard();
   onClick("#debug-button", () => {
     document.querySelector("#debug-buttons-section").classList.toggle("hidden");
